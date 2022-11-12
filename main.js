2c927ef05d651b03a421773564c5460147a0495f
Sakeththecoder13 <86160417+Sakeththecoder13@users.noreply.github.com>
2022-11-11 20:31:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristy = 0;
+
+
+
+song = "";
+
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+    song.setVolume(0.4);
+    song.rate(1);
+}
+
+function modelLoaded(){
+    console.log("Model is loaded!");
+}
+
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist X coordinates:" + leftWristX, "Left wrist Y coordinates:" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist X coordinates:" +rightWristX, "Right wrist Y coordinates:" + rightWristY);
+        
+    }
+}
+
